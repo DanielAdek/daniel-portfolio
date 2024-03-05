9ca8632b20851fc9de46f2bb20b67082cdde7862
DanielAdek <maildaniel.me1@gmail.com>
2024-03-05 21:46:04 +0100

    build portfo

---
+++ src/components/projects/ProjectsFilter.jsx
@@ -0,0 +1,40 @@
+const selectOptions = [
+	'Web Application',
+	'Mobile Application',
+	'Servers only',
+];
+
+function ProjectsFilter({ setSelectProject }) {
+	const handleChange = (event) => setSelectProject(event.target.value);
+
+	return (
+		<select
+			onChange={handleChange}
+			className="px-4
+                sm:px-6
+                py-2
+                border
+                dark:border-secondary-dark
+                rounded-lg
+                text-sm
+                sm:text-md
+                dark:font-medium
+                bg-secondary-light
+                dark:bg-ternary-dark
+                text-primary-dark
+                dark:text-ternary-light"
+		>
+			<option value={""} className="text-sm sm:text-md">
+				All Projects
+			</option>
+
+			{selectOptions.map((option) => (
+				<option className="text-normal sm:text-md" key={option}>
+					{option}
+				</option>
+			))}
+		</select>
+	);
+}
+
+export default ProjectsFilter;
