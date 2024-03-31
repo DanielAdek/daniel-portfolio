8454a0077b642b09ac287540c8beae3f579c00f3
DanielAdek <maildaniel.me1@gmail.com>
2024-03-31 19:37:27 +0100

    update bio

--- src/data/aboutMeData.ts
+++ src/data/aboutMeData.ts
@@ -22,13 +22,25 @@ export const aboutMeData = [
 	},
 	{
 		id: uuidv4(),
-		bio: `Responding to challenges by designing and developing solutions and building web applications aligned to customer's
-					services.
+		bio: `My collaboration, people-centric nature, and compassion have afforded me excellent software development skills 
+		Within the last 5 years, I have also streangthen my development skill in Javascript, Typescript and with growth mindedness 
+		I have indulge in self development for better productivity and more development skills which include Java (Spring Boot) and Flutter for 
+		mobile development.`
+	},
+	{
+		id: uuidv4(),
+		bio: `My other achievements includes applications like loan and savings application, booking system, fintech application with customary banks like 
+		Stanbic IBTC pension app, Guarantee Trust Bank fastlane application and more.`
+	},
+	{
+		id: uuidv4(),
+		bio: `I respond to challenges by designing and developing solutions and building web applications aligned to customer's
+					needs and services.
 				`
 	},
 	{
 		id: uuidv4(),
-		bio: `Translating solutions into code and working across many different APIs, third-party integrations
+		bio: `I translate solutions into code and working across many different APIs, third-party integrations
 					and databases.`
 	},
 ];
